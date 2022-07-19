00485ebb2559dad7cc294905ff0245be1d0a7926
alcirm <alcir@softsa.com.br>
2022-07-19 11:37:53 -0400

    first commit files

---
+++ src/components/Button.tsx
@@ -0,0 +1,21 @@
+import { Button as NativeBaseButton, IButtonProps, Heading } from 'native-base';
+
+type Props = IButtonProps & {
+  title: string;
+};
+
+export function Button({ title, ...rest }: Props) {
+  return (
+    <NativeBaseButton
+      bg='green.700'
+      h={14}
+      fontSize='sm'
+      _pressed={{ bg: 'green.500' }}
+      {...rest}
+    >
+      <Heading color='white' fontSize='md'>
+        {title}
+      </Heading>
+    </NativeBaseButton>
+  );
+}
